7751611dc8c69978f355110743f2d223b702b2af
sh92 <sanghee.lee1992@gmail.com>
2018-07-24 21:19:27 +0900

    Synchronized

---
+++ README.md
@@ -0,0 +1,6 @@
+## Thread Example for java
+
+* Cracking the coding Interview
+* The Site about thread 
+   * Thread Safety [Link](https://www.journaldev.com/1061/thread-safety-in-java)
+* MultiThreading - [Udemy](https://www.udemy.com/java-multithreading/learn/v4/content)
---
+++ Udemy/src/demo5/App.java
@@ -0,0 +1,51 @@
+package demo5;
+
+public class App {
+	private int count;
+
+	public synchronized void increment() {
+		count++;
+	}
+
+	public static void main(String[] args) {
+		App app = new App();
+		app.doWork();
+	}
+
+	private void doWork() {
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 10000; i++)
+					increment();
+			}
+
+		});
+
+		t1.start();
+
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 10000; i++)
+					increment();
+			}
+
+		});
+
+		t2.start();
+
+		try {
+			t1.join();
+			t2.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Count is " + count);
+
+	}
+
+}
